a5b01c4cc4fa9e66dc8acef87a9911e95da54a7b
kibeetfelix <felixkibeet@gmail.com>
2022-12-30 13:54:47 +0300

    creating a function js

--- script.js
+++ script.js
@@ -3,3 +3,13 @@ const hourEl = document.getElementById("hour")
 const minuteEl = document.getElementById("minute")
 const secondEl = document.getElementById("second")
 
+const newYearTime = new Date("Jan 1, 2023 00:00:00").getTime()
+
+updateCountdown()
+
+function updateCountdown() {
+    const now = new Date().getTime();
+    const gap = newYearTime - now
+
+
+}
