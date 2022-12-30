2a74bc8a623c7555f615a7708a97ec19ac64b2bc
kibeetfelix <felixkibeet@gmail.com>
2022-12-30 13:52:24 +0300

    calling elements js

---
+++ script.js
@@ -0,0 +1,5 @@
+const dayEl = document.getElementById("day")
+const hourEl = document.getElementById("hour")
+const minuteEl = document.getElementById("minute")
+const secondEl = document.getElementById("second")
+
